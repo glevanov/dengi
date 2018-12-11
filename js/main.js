1d1205efe7313cc4823381740c866d2a522fcaaf
Grigoriy Levanov <glevanov@gmail.com>
2018-12-11 22:28:14 +0300

    Reset project, add parcel

--- js/main.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-(function () {
-    const totalField = document.querySelector('.total');
-
-    totalField.textContent = `${window.data.total.currency} ${window.data.total.value}`
-})();
