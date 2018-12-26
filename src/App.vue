3347010e691dfff9d6ec5fa269edbc069b2ed817
Grigoriy Levanov <glevanov@gmail.com>
2018-12-26 15:30:53 +0300

    Add USD entries to accounts

--- src/App.vue
+++ src/App.vue
@@ -18,28 +18,58 @@
     data: () => ({
       accounts: [
         {
-          currency: '₽',
+          currency: {
+            code: 'RUB',
+            text: '₽'
+          },
           custodian: 'Тиньков',
           type: 'Вклад',
           value: 1400000
         },
         {
-          currency: '₽',
+          currency: {
+            code: 'RUB',
+            text: '₽'
+          },
           custodian: 'Альфа',
           type: 'Вклад',
           value: 1034102.45
         },
         {
-          currency: '₽',
+          currency: {
+            code: 'RUB',
+            text: '₽'
+          },
           custodian: 'Альфа-Директ',
           type: 'ETF',
           value: 2201823.13
         },
         {
-          currency: '₽',
+          currency: {
+            code: 'RUB',
+            text: '₽'
+          },
           custodian: 'Альфа',
           type: 'ПИФ',
           value: 876403.35
+        },
+        {
+          currency: {
+            code: 'USD',
+            text: '$'
+          },
+          custodian: 'Альфа',
+          type: 'ETF',
+          value: 10223.11
+        },
+        {
+          currency: {
+            code: 'USD',
+            text: '$'
+          },
+          custodian: 'Тиньков',
+          type: 'Вклад',
+          value: 1031.07
         }
       ]
     })
