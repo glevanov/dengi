236ba7905b649388afe9fe6091316a9b14f508b7
Grigoriy Levanov <glevanov@gmail.com>
2019-01-20 20:32:12 +0300

    Remove test data from store

--- src/store/store.js
+++ src/store/store.js
@@ -36,46 +36,6 @@ export default new Vuex.Store({
       EUR: 79.4605,
     },
     isAccountModalVisible: false,
-    accounts: [
-
-    ],
+    accounts: [],
   },
 });
-
-/*
-{
-  currency: 'RUB',
-    custodian: 'Тиньков',
-  type: 'Вклад',
-  value: 1400000,
-},
-{
-  currency: 'RUB',
-    custodian: 'Альфа',
-  type: 'Вклад',
-  value: 1034102.45,
-},
-{
-  currency: 'USD',
-    custodian: 'Альфа',
-  type: 'ETF',
-  value: 10223.11,
-},
-{
-  currency: 'USD',
-    custodian: 'Тиньков',
-  type: 'Вклад',
-  value: 1031.07,
-},
-{
-  currency: 'EUR',
-    custodian: 'Открытие',
-  type: 'Вклад',
-  value: 2030.81,
-},
-{
-  currency: 'EUR',
-    custodian: 'МКБ',
-  type: 'Вклад',
-  value: 304.37,
-},*/
