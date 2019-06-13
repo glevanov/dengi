a28771d8fb20b85d9afe04e3cc5deb50cc09a21c
Grigoriy Levanov <glevanov@gmail.com>
2019-06-13 22:19:59 +0300

    Formats total

--- src/components/Total/Total.js
+++ src/components/Total/Total.js
@@ -2,13 +2,15 @@ import React from 'react';
 import store from 'store';
 import {observer} from 'mobx-react';
 import {decorate, computed} from 'mobx';
-import {getTotalRUB, getTotalUSD , getTotalEUR} from 'currency';
+import {getTotalRUB, getTotalUSD, getTotalEUR, formatNum} from 'currency';
 
 export default class Total extends React.Component {
   get total() {
-    return getTotalRUB(store.accounts)
+    return formatNum(
+      getTotalRUB(store.accounts)
       + getTotalUSD(store.accounts, store.rates.USD)
-      + getTotalEUR(store.accounts, store.rates.EUR);
+      + getTotalEUR(store.accounts, store.rates.EUR)
+    );
   }
   render() {
     return (
