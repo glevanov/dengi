cbd7fb35a4b41fd9a27253afb15766d069911fa2
Grigoriy Levanov <glevanov@gmail.com>
2019-06-14 21:55:31 +0300

    Extract account details to separate component

---
+++ src/components/AccountDetails/AccountDetails.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './AccountDetails.css';
+import {formatNum} from 'currency';
+
+AccountDetails.propTypes = {
+  amount: PropTypes.number.isRequired,
+  type: PropTypes.string,
+  custodian: PropTypes.string,
+  elementClass: PropTypes.string,
+};
+
+export default function AccountDetails(
+  {
+    amount,
+    type,
+    custodian,
+    elementClass,
+  }
+) {
+  let classes = 'account-details';
+  if (elementClass) {
+    classes += ` ${elementClass}`;
+  }
+
+  return (
+    <p className={classes}>
+      <span className="account-details__amount">{formatNum(amount)}</span>
+      <span className="account-details__details">{type} в {custodian}</span>
+    </p>
+  )
+}
