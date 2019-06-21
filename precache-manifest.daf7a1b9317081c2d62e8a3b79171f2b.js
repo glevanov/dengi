3be08c69a80f48164a9b0cf7ebbca09b5ff7a71e
Grigoriy Levanov <glevanov@gmail.com>
2019-06-21 22:16:50 +0300

    Updates

---
+++ precache-manifest.daf7a1b9317081c2d62e8a3b79171f2b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a041973fdfd4255c1f61d23a11680090",
+    "url": "/dengi/static/media/edit-button.a041973f.svg"
+  },
+  {
+    "revision": "c16cc5044af4d6d73016",
+    "url": "/dengi/static/css/main.acbab754.chunk.css"
+  },
+  {
+    "revision": "97a0273508766527abc9",
+    "url": "/dengi/static/js/runtime~main.d82d2505.js"
+  },
+  {
+    "revision": "1d59ac9ecf33fe9fcf924c5ae6cdd2da",
+    "url": "/dengi/static/media/EUR.1d59ac9e.svg"
+  },
+  {
+    "revision": "8945ca5902a29f726558",
+    "url": "/dengi/static/js/2.e42ebbbf.chunk.js"
+  },
+  {
+    "revision": "c16cc5044af4d6d73016",
+    "url": "/dengi/static/js/main.2f76d5fa.chunk.js"
+  },
+  {
+    "revision": "1e0a35ed52eaf2b27d9c44d565f9c747",
+    "url": "/dengi/static/media/delete-button.1e0a35ed.svg"
+  },
+  {
+    "revision": "a9d45a22cc0f69015c0fd3e7eedb5d3e",
+    "url": "/dengi/static/media/RUB.a9d45a22.svg"
+  },
+  {
+    "revision": "58921271b760494bc2994b1ea6266ff9",
+    "url": "/dengi/static/media/USD.58921271.svg"
+  },
+  {
+    "revision": "8945ca5902a29f726558",
+    "url": "/dengi/static/css/2.be2aa532.chunk.css"
+  },
+  {
+    "revision": "038cdb89735e9bb370d520dadfbffb5b",
+    "url": "/dengi/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/dengi/precache-manifest.daf7a1b9317081c2d62e8a3b79171f2b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/dengi/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
